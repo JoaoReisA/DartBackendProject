5a1f2dd748c9b73072401804159c0123f11eb36e
João Victor Reis Alves - JAV <joao.alves@iteris.com.br>
2022-06-29 14:44:12 -0300

    init project

---
+++ bin/shelf_project.dart
@@ -0,0 +1,5 @@
+import 'package:shelf_project/shelf_project.dart' as shelf_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shelf_project.calculate()}!');
+}
---
+++ test/shelf_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:shelf_project/shelf_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
